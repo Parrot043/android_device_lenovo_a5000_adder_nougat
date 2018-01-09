cd2b2ee55912e0a0c26246704f4dc063845636de
Илья Ковальчук <samsung.fly64@gmail.com>
2018-01-09 16:02:13 +0300

    fix adder

--- lineage.mk
+++ lineage.mk
@@ -5,16 +5,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Call device specific makefile
-$(call inherit-product, device/highscreen/giraffe/lineage_giraffe.mk)
+$(call inherit-product, device/lenovo/adder/lineage_adder.mk)
 
-LOCAL_PATH := device/highscreen/giraffe
+LOCAL_PATH := device/lenovo/adder
 
 # Common CM stuff
-CM_BUILD := giraffe
+CM_BUILD := adder
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=7.1.2/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="giraffe-user 7.1.2 MRA58M 2280749 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=7.1.2/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="adder-user 7.1.2 MRA58M 2280749 release-keys"
 
 PRODUCT_NAME := lineage_adder
 PRODUCT_DEVICE := adder
@@ -23,7 +23,7 @@ PRODUCT_MANUFACTURER := lenovo
 PRODUCT_MODEL := A5000
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="giraffe"
+    PRODUCT_DEVICE="adder"
 
 # Correct bootanimation size for the screen
 TARGET_SCREEN_HEIGHT := 1280
